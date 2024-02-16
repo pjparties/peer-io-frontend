ed40033aa6013711812c0ebd789f3b54f82e038e
parth <parthhjuneja@gmail.com>
2024-02-16 20:47:41 +0530

    added olive to tailwind module export config

--- src/components/AuthButton.jsx
+++ src/components/AuthButton.jsx
@@ -4,7 +4,7 @@ const AuthButton = ({title, target}) => {
   return (
     // responsive login button blue color fixed size
     <button
-      className="h-12 w-32 rounded-lg bg-[#416D19] font-bold text-white"
+      className="h-12 w-32 rounded-lg bg-olive font-bold text-white"
       onClick={() => {
         alert(`${title} button clicked`);
       }}
--- src/pages/Landing.page.jsx
+++ src/pages/Landing.page.jsx
@@ -8,7 +8,7 @@ const LandingPage = () => {
       <div className="h-full w-3/5 border-r-8 bg-yellow-200">
         <LeftHero />
       </div>
-      <div className="flex h-full w-2/5 flex-col items-center">
+      <div className="flex h-full w-2/5 flex-col items-center bg-gray-200">
         <Auth />
       </div>
     </div>
