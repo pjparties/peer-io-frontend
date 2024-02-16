9aff58a6275abf3c577636b02cc03e0d8f0666f5
areenoverclouds <kaurareen@gmail.com>
2024-02-16 20:57:12 +0530

    remove login from prefernce page

--- src/pages/Preferences.page.jsx
+++ src/pages/Preferences.page.jsx
@@ -6,7 +6,7 @@ import { Selector } from "@/components/Selector";
 import Chat from './Chat.page.jsx'
 
 const PreferencePage = () => {
-  const [show, setShow] = useState('selector'); // 3 options: login, selector, chat
+  const [show, setShow] = useState('selector'); // 3 options: selector, chat
 
   const startChatting = () => {
     setShow('chat');
@@ -15,7 +15,6 @@ const PreferencePage = () => {
   return (
     <div className="flex flex-col w-full h-screen">
       <Navbar />
-      {show === 'login' && <Auth/>}
       {show === 'selector' && <Selector startChatting={startChatting}/>}
       {show === 'chat' && <Chat />}
       <Footer />
