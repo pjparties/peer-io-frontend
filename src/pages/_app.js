6b24e3d3848003dcc8e3e257f775e6ad340c3b44
parth <parthhjuneja@gmail.com>
2024-02-26 02:02:42 +0530

    context wrapper

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,10 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { SocketProvider } from "@/contexts/socketProvider";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <SocketProvider id="123">
+      <Component {...pageProps} />
+    </SocketProvider>
+  );
 }
