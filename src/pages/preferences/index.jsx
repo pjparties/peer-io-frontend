be2b3a93adb17fc79baf524b373e92d0097721c9
areenoverclouds <kaurareen@gmail.com>
2024-02-26 23:13:12 +0530

    css changes and refactor

--- src/pages/preferences/index.jsx
+++ src/pages/preferences/index.jsx
@@ -1,23 +1,25 @@
 import LeftHero from "@/components/LeftHero.jsx";
-import Preferences from "@/components/Preferences.jsx";
+import { Selector } from "@/components/Selector.jsx";
 
 const PreferencePage = () => {
   return (
     <div className="main container flex flex-row">
-      <div className="bg-primary h-full w-3/5">
+      <div className="h-full w-3/5 bg-primary">
         <LeftHero />
       </div>
-      <div className="bg-secondary flex w-2/5 flex-col items-center justify-center border-l-4">
+      <div className="flex w-2/5 flex-col items-center justify-center border-l-4 bg-secondary">
         <div className="text-wrapper mb-16 flex flex-col px-6 text-center">
           <h1 className=" text-4xl font-bold text-white ">
             Select Your Preferences
           </h1>
           <p className="mt-2 text-lg font-normal text-white">
-            Choose at least one to get started with finding
-            people that share your interests.
+            Choose at least one to get started with finding people that share
+            your interests.
           </p>
         </div>
-        <Preferences />
+        <div className="main-preferences-wrapper">
+          <Selector />
+        </div>
       </div>
     </div>
   );
