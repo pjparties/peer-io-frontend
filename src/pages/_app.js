64221261037bce77e407e5b6576580ef88da5a03
parth <parthhjuneja@gmail.com>
2024-02-29 00:06:16 +0530

    push before tata goodnight hehe set id usereffect and persistence in peerio-id needed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,19 @@
 import "@/styles/globals.css";
+import { useEffect, useState } from "react";
 import { SocketProvider } from "@/contexts/socketProvider";
 import { SessionProvider } from "next-auth/react"
 import useLocalStorage from "@/hooks/useLocalStorage";
+import {v4 as uuidV4} from "uuid";
+
+
 
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
-  const [id, setId] = useLocalStorage("id");
+  const initialValue = (uuidV4());
+  const [id,setId] = useLocalStorage("peerio-id", initialValue);
+  useEffect(() => {
+    setId(initialValue);
+  }, []);
+  console.log(id);
   return (
     <SessionProvider>
       <SocketProvider id={id}>
