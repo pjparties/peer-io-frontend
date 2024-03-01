ea8912c8d3783f711bc33493a7b4b1018b09cc30
parth <parthhjuneja@gmail.com>
2024-03-01 21:04:54 +0530

    test branch

---
+++ src/components/ChatRoom.jsx
@@ -0,0 +1,15 @@
+// ChatRoom.js
+
+import React, { useState, useEffect } from "react";
+import io from "socket.io-client";
+
+const ChatRoom = () => {
+  
+  return (
+    <div>
+      <h1>Chat Room</h1>
+    </div>
+  );
+};
+
+export default ChatRoom;
